99bffd01aa0d5b97f6042fcc97b1657feee7a7a2
idisrajesh <rajricarp@gmail.com>
2022-07-19 12:08:14 +0530

    no message

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import logo from './logo.svg';
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import { loadModules } from "esri-loader";
-import { Map } from "@esri/react-arcgis";
+import MapComponent from './component/map/map';
 function App() {
-  const handleMapLoad = function (map, view) {
-    loadModules(["esri/widgets/Search","esri/widgets/Home"]).
-    then(([Search,Home,GeoJSONLayer]) => {
-      const searchWidget = new Search({
-        view: view
-      });
-      const homeBtn = new Home({
-        view: view
-      });
-      view.ui.add(homeBtn, "top-left");
-      view.ui.add(searchWidget, {
-        position: "top-right"
-      });
-    });
-  };
 
   return (
     <div  className='App' style={{ width: '100vw', height: '100vh' }}>
-      <Map/>
+      <MapComponent/>
     </div>
     
       
